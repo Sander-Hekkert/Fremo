70782c036554ea7469d15d88cf0e11b31531ff67
Sander-Hekkert <47881@hoornbeeck.nl>
2023-11-16 13:13:26 +0100

    typ fout eruit gehaald

--- fremo/resources/views/home.blade.php
+++ fremo/resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
                             <img src="{{ asset('Fotos/image2.png') }}" alt="home" class="img-fluid mx-auto d-block mb-0">
-                    <p>Wij, die begin jaren '80 FREMO hebben opgericht, vonden, dat de mogelijkheden om zich met de modelbouwhobby bezig te houden nog lang niet waren uitgeput. Wie zich als individu met deze interressante hobby bezig houdt, stoot, bij een gemiddelde hoeveelheid aan beschikbare tijd, geld en plaats binnen zijn eigen muren al snel aan de grenzen van zijn mogelijkheden. Als lid van een Ťgewoneť modelbaanvereniging, moet men zich vaak tevreden stellen met een al vastgelegde thematiek die normaal gesproken gebaseerd is op een jarenlang bevochten compromis.</p>
+                    <p>Wij, die begin jaren '80 FREMO hebben opgericht, vonden, dat de mogelijkheden om zich met de modelbouwhobby bezig te houden nog lang niet waren uitgeput. Wie zich als individu met deze interressante hobby bezig houdt, stoot, bij een gemiddelde hoeveelheid aan beschikbare tijd, geld en plaats binnen zijn eigen muren al snel aan de grenzen van zijn mogelijkheden. Als lid van een modelbaanvereniging, moet men zich vaak tevreden stellen met een al vastgelegde thematiek die normaal gesproken gebaseerd is op een jarenlang bevochten compromis.</p>
                 </div>
             </div>
                         <div class="card">
