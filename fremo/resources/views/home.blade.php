efcf528180be5e8153ff0827a3a707a75b8e3a06
Boaz Stip <47250@hoornbeeck.nl>
2023-11-20 09:19:47 +0100

    cards deleted in home

--- fremo/resources/views/home.blade.php
+++ fremo/resources/views/home.blade.php
@@ -4,10 +4,8 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Over ons') }}</div>
-
-                <div class="card-body">
+                <div class="">{{ __('Over ons') }}</div>
+                <div class="">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -16,11 +14,11 @@
                             <img src="{{ asset('Fotos/image2.png') }}" alt="home" class="img-fluid mx-auto d-block mb-0">
                     <p>Wij, die begin jaren '80 FREMO hebben opgericht, vonden, dat de mogelijkheden om zich met de modelbouwhobby bezig te houden nog lang niet waren uitgeput. Wie zich als individu met deze interressante hobby bezig houdt, stoot, bij een gemiddelde hoeveelheid aan beschikbare tijd, geld en plaats binnen zijn eigen muren al snel aan de grenzen van zijn mogelijkheden. Als lid van een modelbaanvereniging, moet men zich vaak tevreden stellen met een al vastgelegde thematiek die normaal gesproken gebaseerd is op een jarenlang bevochten compromis.</p>
                 </div>
-            </div>
-                        <div class="card">
-                <div class="card-header">{{ __('Over FREMO') }}</div>
 
-                <div class="card-body">
+                <div class="">
+                <div class="">{{ __('Over FREMO') }}</div>
+
+                <div class="">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
@@ -29,8 +27,6 @@
                     <p>Wilt u meer weten over wie wij zijn, wat wij doen en waarom dat zo is? Kijk dan dit Filmpje.</p>
                     <iframe width="560" height="315" src="https://www.youtube.com/embed/uDsWfdaHBvg" frameborder="0" allowfullscreen></iframe>
                 </div>
-            </div>
-        </div>
     </div>
 </div>
 @endsection
