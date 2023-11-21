5e3838224509548b207ea8f6b3159f0decca5da6
Sander-Hekkert <47881@hoornbeeck.nl>
2023-11-21 16:29:05 +0100

    dienstregeling niet meer verpest

---
+++ fremo/resources/views/project/dienstregeling/index.blade.php
@@ -0,0 +1,38 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row g-0">
+            <div class="col-2">
+
+                <a href="{{ route('project.create') }}" class="btn btn-primary">
+                    New
+                </a>
+            </div>
+            <div class="col-8 offset-1">
+                <table class="table">
+                    <thead>
+                    <h1 class="mt-5 mb-3">Projectbeheer</h1>
+                    <tr>
+                        <th>Naam</th>
+                        <th>Aangemaakt op</th>
+                        <th>Laatst geopend</th>
+                        <th>Status</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach ($projects as $project)
+                        <tr>
+                            <td>{{ $project->titel }}</td>
+                            <td>{{ $project->created_at }}</td>
+                            <td>{{ $project->updated_at }}</td>
+                            <td>{{ $project->status }}</td>
+                          
+                        </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
--- fremo/resources/views/project/index.blade.php
+++ fremo/resources/views/project/index.blade.php
@@ -1,37 +1,15 @@
+<!-- create.blade.php -->
 @extends('layouts.app')
 
 @section('content')
     <div class="container">
-        <form action="{{ route('projects.store') }}" method="POST">
+        <form action="{{ route('project.store') }}" method="POST">
             @csrf
             <div class="form-group">
                 <label for="titel">Titel:</label>
                 <input type="text" name="titel" id="titel" class="form-control">
             </div>
-            <div class="col-8 offset-1">
-                <table class="table">
-                    <thead>
-                    <h1 class="mt-5 mb-3">Projectbeheer</h1>
-                    <tr>
-                        <th>Naam</th>
-                        <th>Aangemaakt op</th>
-                        <th>Laatst geopend</th>
-                        <th>Status</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    @foreach ($projects as $project)
-                        <tr>
-                            <td>{{ $project->titel }}</td>
-                            <td>{{ $project->created_at }}</td>
-                            <td>{{ $project->updated_at }}</td>
-                            <td>{{ $project->status }}</td>
-                          
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
+            <button type="submit" class="btn btn-primary">Opslaan</button>
+        </form>
     </div>
 @endsection
