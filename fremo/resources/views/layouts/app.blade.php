aa3bcaba98e215ac9162d63d5a8c1aaee7cd7313
lorenzo <lorenzoneels@gmail.com>
2023-11-21 15:45:54 +0100

    navbar geupdate

--- fremo/resources/views/layouts/app.blade.php
+++ fremo/resources/views/layouts/app.blade.php
@@ -19,24 +19,23 @@
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md bg-white">
-            <div class="container">
+            <div class="container-fluid g-0">
+
                 <!-- Replace the text with an image -->
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="{{ asset('Fotos/logo.png') }}" alt="Rails" class="img-fluid mx-auto d-block mb-0" style="width: 30%; max-height: 30%;">
+                <a class="navbar-brand ms-5" href="{{ url('/') }}">
+                    <img src="{{ asset('Fotos/logo.png') }}" alt="Rails" class="img-fluid my-3 d-block " style="width: 100px;">
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav me-auto">
 
-                    </ul>
 
-                    <!-- Right Side Of Navbar -->
-                    <ul class="navbar-nav ms-auto">
-                        <!-- Authentication Links -->
+
+
+                    <ul class="navbar-nav ms-auto d-flex align-items-center me-5">
+
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -61,19 +60,19 @@
                                     <a class="nav-link" href="{{ route('dienstregeling') }}">{{ __('Dienstregeling') }}</a>
                                 </li>
                             @endif
-                    <li class="nav-item dropdown">
-                       <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ 'Spoorbeheer' }}
                                 </a>
-                        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            @if (Route::has('module.index'))
-                                <a class="dropdown-item" href="{{ route('module.index') }}">{{ __('Module') }}</a>
-                            @endif
-                            @if (Route::has('trein.index'))
-                                <a class="dropdown-item" href="{{ route('trein.index') }}">{{ __('Treinen') }}</a>
-                            @endif
-                        </div>
-                    </li>
+                                <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    @if (Route::has('module.index'))
+                                        <a class="dropdown-item" href="{{ route('module.index') }}">{{ __('Module') }}</a>
+                                    @endif
+                                    @if (Route::has('trein.index'))
+                                        <a class="dropdown-item" href="{{ route('trein.index') }}">{{ __('Treinen') }}</a>
+                                    @endif
+                                </div>
+                            </li>
                             @if (Route::has('wagenkaart'))
                                 <li class="nav-item">
                                     <a class="nav-link" href="{{ route('wagenkaart') }}">{{ __('Wagenkaart') }}</a>
@@ -87,7 +86,7 @@
                                 <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
+                                            document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
 
@@ -101,6 +100,7 @@
                 </div>
             </div>
         </nav>
+
         <img src="{{ asset('Fotos/rails.svg') }}" alt="Rails" class="img-fluid mx-auto d-block mb-0" style="width: 100%; max-height: 200px;">
 
         <main class="py-4">
