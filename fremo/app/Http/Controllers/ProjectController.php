a9bea8a628db3586f91db89778f94fe167c67edb
Boaz Stip <47250@hoornbeeck.nl>
2023-11-23 10:02:52 +0100

    projects fix

--- fremo/app/Http/Controllers/ProjectController.php
+++ fremo/app/Http/Controllers/ProjectController.php
@@ -2,24 +2,24 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Project;
+use App\Models\Projects;
 
 class ProjectController extends Controller
 {
     public function store(Request $request)
     {
         // Validate and store the project...
-        
+
         // After successfully storing the project, redirect to wegdiagram.create
-        $project = new Project(); // Assuming you have a new project instance
-    
+        $project = new Projects(); // Assuming you have a new project instance
+
         // Save the project to get its ID
         $project->titel = $request->input('titel');
         $project->users_id = auth()->id();
         $project->datum_en_tijd_aangemaakt = now();
         $project->status = '25%';
         $project->save();
-    
+
         // Redirect to wegdiagram.create with the project_id parameter
         return redirect()->route('wegdiagram.create', ['project_id' => $project->id])
                          ->with('success', 'Project successfully created!');
@@ -27,7 +27,7 @@ class ProjectController extends Controller
     public function index()
     {
         // Your logic to fetch and display projects goes here
-        $projects = Project::all(); // Fetching all projects
+        $projects = Projects::all(); // Fetching all projects
         return view('project.index', compact('projects'));
     }
 }
