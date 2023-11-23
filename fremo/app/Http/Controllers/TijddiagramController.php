4c4f02a75b21c0feae0c8a1eefedcac4cd9be33e
Boaz Stip <47250@hoornbeeck.nl>
2023-11-23 10:26:22 +0100

    tijddiagram dingen

--- fremo/app/Http/Controllers/TijddiagramController.php
+++ fremo/app/Http/Controllers/TijddiagramController.php
@@ -1,10 +1,34 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Tijddiagram;
 
 use Illuminate\Http\Request;
 
 class TijddiagramController extends Controller
 {
-    //
+   /* public function store(Request $request)
+    {
+        // Validate and store the project...
+
+        // After successfully storing the project, redirect to wegdiagram.create
+        $project = new Projects(); // Assuming you have a new project instance
+
+        // Save the project to get its ID
+        $project->titel = $request->input('titel');
+        $project->users_id = auth()->id();
+        $project->datum_en_tijd_aangemaakt = now();
+        $project->status = '25%';
+        $project->save();
+
+        // Redirect to wegdiagram.create with the project_id parameter
+        return redirect()->route('wegdiagram.create', ['project_id' => $project->id])
+            ->with('success', 'Project successfully created!');
+    }*/
+    public function index()
+    {
+        // Your logic to fetch and display projects goes here
+        $projects = Tijddiagram::all(); // Fetching all projects
+        return view('project.index', compact('projects'));
+    }
 }
