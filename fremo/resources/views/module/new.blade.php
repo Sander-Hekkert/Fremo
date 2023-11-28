71c3c6e69b9bc4b0503223638998bdbec19a6549
lorenzo <lorenzoneels@gmail.com>
2023-11-28 14:31:18 +0100

    Module mooi gemaakt

--- fremo/resources/views/module/new.blade.php
+++ fremo/resources/views/module/new.blade.php
@@ -1,35 +1,43 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Create New Module</h1>
+    <div class="container">
+        <h1 class="mt-2">Create New Module</h1>
 
-    <form action="{{ route('module.store') }}" method="POST">
-        @csrf
+        <form action="{{ route('module.store') }}" method="POST" class="mt-4">
+            @csrf
 
-        <label for="naam_bezitter">Naam Bezitter:</label>
-        <input type="text" name="naam_bezitter" required>
-        <br>
+            <div class="mb-2">
+                <label for="naam_bezitter" class="form-label">Naam Bezitter:</label>
+                <input type="text" name="naam_bezitter" class="form-control" required>
+            </div>
 
-        <label for="nummer_module">Nummer Module:</label>
-        <input type="text" name="nummer_module" required>
-        <br>
+            <div class="mb-2">
+                <label for="nummer_module" class="form-label">Nummer Module:</label>
+                <input type="text" name="nummer_module" class="form-control" required>
+            </div>
 
-        <label for="e_f">E-F:</label>
-        <input type="text" name="e_f" required>
-        <br>
+            <div class="mb-2">
+                <label for="e_f" class="form-label">E-F:</label>
+                <input type="text" name="e_f" class="form-control" required>
+            </div>
 
-        <label for="opmerkingen_bijzonderheden">Opmerkingen Bijzonderheden:</label>
-        <textarea name="opmerkingen_bijzonderheden" required></textarea>
-        <br>
+            <div class="mb-2">
+                <label for="opmerkingen_bijzonderheden" class="form-label">Opmerkingen Bijzonderheden:</label>
+                <textarea name="opmerkingen_bijzonderheden" class="form-control" rows="3" required></textarea>
+            </div>
 
-        <label for="aantal_sporen_begin">Aantal Sporen Begin:</label>
-        <input type="text" name="aantal_sporen_begin" required>
-        <br>
+            <div class="mb-2">
+                <label for="aantal_sporen_begin" class="form-label">Aantal Sporen Begin:</label>
+                <input type="text" name="aantal_sporen_begin" class="form-control" required>
+            </div>
 
-        <label for="aantal_sporen_eind">Aantal Sporen Eind:</label>
-        <input type="text" name="aantal_sporen_eind" required>
-        <br>
-        
-        <button type="submit">Create Module</button>
-    </form>
+            <div class="mb-2">
+                <label for="aantal_sporen_eind" class="form-label">Aantal Sporen Eind:</label>
+                <input type="text" name="aantal_sporen_eind" class="form-control" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary">Create Module</button>
+        </form>
+    </div>
 @endsection
