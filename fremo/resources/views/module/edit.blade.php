d59b1521d5038ebca1580ab8318583ffbd7d0cf2
lorenzo <lorenzoneels@gmail.com>
2023-11-28 14:51:09 +0100

    Trein en module edit mooi gemaakt

--- fremo/resources/views/module/edit.blade.php
+++ fremo/resources/views/module/edit.blade.php
@@ -1,36 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Edit Module</h1>
+    <div class="container mt-5">
+        <h1>Edit Module</h1>
 
-    <form action="{{ route('module.update', $module->id) }}" method="POST">
-        @csrf
-        @method('PUT')
+        <form action="{{ route('module.update', $module->id) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-        <label for="naam_bezitter">Naam Bezitter:</label>
-        <input type="text" name="naam_bezitter" value="{{ $module->naam_bezitter }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="naam_bezitter" class="form-label">Naam Bezitter:</label>
+                <input type="text" name="naam_bezitter" value="{{ $module->naam_bezitter }}" class="form-control" required>
+            </div>
 
-        <label for="nummer_module">Nummer Module:</label>
-        <input type="text" name="nummer_module" value="{{ $module->nummer_module }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="nummer_module" class="form-label">Nummer Module:</label>
+                <input type="text" name="nummer_module" value="{{ $module->nummer_module }}" class="form-control" required>
+            </div>
 
-        <label for="e_f">E-F:</label>
-        <input type="text" name="e_f" value="{{ $module->e_f }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="e_f" class="form-label">E-F:</label>
+                <input type="text" name="e_f" value="{{ $module->e_f }}" class="form-control" required>
+            </div>
 
-        <label for="opmerkingen_bijzonderheden">Opmerkingen Bijzonderheden:</label>
-        <textarea name="opmerkingen_bijzonderheden" required>{{ $module->opmerkingen_bijzonderheden }}</textarea>
-        <br>
+            <div class="mb-3">
+                <label for="opmerkingen_bijzonderheden" class="form-label">Opmerkingen Bijzonderheden:</label>
+                <textarea name="opmerkingen_bijzonderheden" class="form-control" required>{{ $module->opmerkingen_bijzonderheden }}</textarea>
+            </div>
 
-        <label for="aantal_sporen_begin">Aantal Sporen Begin:</label>
-        <input type="text" name="aantal_sporen_begin" value="{{ $module->aantal_sporen_begin }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="aantal_sporen_begin" class="form-label">Aantal Sporen Begin:</label>
+                <input type="text" name="aantal_sporen_begin" value="{{ $module->aantal_sporen_begin }}" class="form-control" required>
+            </div>
 
-       <label for="aantal_sporen_eind">Aantal Sporen Eind:</label>
-        <input type="text" name="aantal_sporen_eind" value="{{ $module->aantal_sporen_eind }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="aantal_sporen_eind" class="form-label">Aantal Sporen Eind:</label>
+                <input type="text" name="aantal_sporen_eind" value="{{ $module->aantal_sporen_eind }}" class="form-control" required>
+            </div>
 
-        <button type="submit">Update Module</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Update Module</button>
+        </form>
+    </div>
 @endsection
--- fremo/resources/views/trein/edit.blade.php
+++ fremo/resources/views/trein/edit.blade.php
@@ -1,36 +1,44 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>Edit Trein</h1>
+    <div class="container">
+        <h1 class="mt-5">Edit Trein</h1>
 
-    <form action="{{ route('trein.update', $trein->id) }}" method="POST">
-        @csrf
-        @method('PUT')
+        <form action="{{ route('trein.update', $trein->id) }}" method="POST">
+            @csrf
+            @method('PUT')
 
-        <label for="naam_bezitter">Naam Bezitter:</label>
-        <input type="text" name="naam_bezitter" value="{{ $trein->naam_bezitter }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="naam_bezitter" class="form-label">Naam Bezitter:</label>
+                <input type="text" name="naam_bezitter" value="{{ $trein->naam_bezitter }}" class="form-control" required>
+            </div>
 
-        <label for="afkorting">Afkorting:</label>
-        <input type="text" name="afkorting" value="{{ $trein->afkorting }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="afkorting" class="form-label">Afkorting:</label>
+                <input type="text" name="afkorting" value="{{ $trein->afkorting }}" class="form-control" required>
+            </div>
 
-        <label for="nummer_trein">Nummer Trein:</label>
-        <input type="text" name="nummer_trein" value="{{ $trein->nummer_trein }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="nummer_trein" class="form-label">Nummer Trein:</label>
+                <input type="text" name="nummer_trein" value="{{ $trein->nummer_trein }}" class="form-control" required>
+            </div>
 
-        <label for="code_trein">Code Trein:</label>
-        <input type="text" name="code_trein" value="{{ $trein->code_trein }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="code_trein" class="form-label">Code Trein:</label>
+                <input type="text" name="code_trein" value="{{ $trein->code_trein }}" class="form-control" required>
+            </div>
 
-        <label for="omschrijving">Omschrijving:</label>
-        <input type="text" name="omschrijving" value="{{ $trein->omschrijving }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="omschrijving" class="form-label">Omschrijving:</label>
+                <input type="text" name="omschrijving" value="{{ $trein->omschrijving }}" class="form-control" required>
+            </div>
 
-        <label for="snelheid">Snelheid:</label>
-        <input type="text" name="snelheid" value="{{ $trein->snelheid }}" required>
-        <br>
+            <div class="mb-3">
+                <label for="snelheid" class="form-label">Snelheid:</label>
+                <input type="text" name="snelheid" value="{{ $trein->snelheid }}" class="form-control" required>
+            </div>
 
-        <button type="submit">Update Trein</button>
-    </form>
+            <button type="submit" class="btn btn-primary">Update Trein</button>
+        </form>
+    </div>
 @endsection
