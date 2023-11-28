1942c4ed222daa8bf5977baeaed77ecaf94ef48a
Sander-Hekkert <47881@hoornbeeck.nl>
2023-11-28 13:54:31 +0100

    Bezig met wegdiagram laatste dingetjes

--- fremo/resources/views/wegdiagram/create.blade.php
+++ fremo/resources/views/wegdiagram/create.blade.php
@@ -13,7 +13,7 @@
                     <input type="hidden" name="project_id" value="{{ $project_id }}">
 
                     <div class="mb-3">
-                        <label for="datum" class="form-label">Datum Tijd / weg diagram:</label>
+                        <label for="datum" class="form-label">Datum:</label>
                         <input type="date" name="datum" class="form-control border border-primary" required>
                     </div>
 
@@ -26,30 +26,17 @@
                         </select>
                     </div>
 
-                    <div class="mb-3">
-                        <label for="trein" class="form-label">Selecteer een trein:</label>
-                        <select name="trein" class="form-select border border-primary">
-                            @foreach($treinen as $trein)
-                                <option value="{{ $trein->id }}">{{ $trein->naam }}</option>
-                            @endforeach
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="trein" class="form-label">Hernoem de trein:</label>
-                        <input type="trein" name="datum" class="form-control border border-primary" required>
-                    </div>
-
                     <div class="row">
                         <div class="col-6">
                             <div class="mb-3">
                                 <label for="starttijd" class="form-label">Starttijd:</label>
-                                <input type="datetime-local" name="starttijd" class="form-control border border-primary" required>
+                                <input type="time" name="starttijd" class="form-control border border-primary" required>
                             </div>
                         </div>
                         <div class="col-6">
                             <div class="mb-3">
                                 <label for="eindtijd" class="form-label">Eindtijd:</label>
-                                <input type="datetime-local" name="eindtijd" class="form-control border border-primary" required>
+                                <input type="time" name="eindtijd" class="form-control border border-primary" required>
                             </div>
                         </div>
                     </div>
@@ -59,14 +46,6 @@
             </div>
             <div class="col-8">
                 <div class="row">
-                    <div class="col">
-                        <div class="card mb-3">
-                            <div class="card-body">
-                                <h5 class="card-title">Treinen</h5>
-
-                            </div>
-                        </div>
-                    </div>
                     <div class="col">
                         <div class="card">
                             <div class="card-body">
